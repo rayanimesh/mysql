420ccecad92fa7858ca433426885d7ac42ae66d4
rayanimesh <mail.rayanimesh@gmail.com>
2020-03-11 09:48:44 +0530

    Test checks

--- .travis/complie_check.sh
+++ .travis/complie_check.sh
@@ -5,6 +5,14 @@ dist_list=$(go tool dist list)
 for dist in ${dist_list}; do
     GOOS=$(echo ${dist} | cut  -d "/" -f 1)
     GOARCH=$(echo ${dist} | cut -d "/" -f 2)
+    echo "Checking compile support for ${GOOS}/${GOARCH}"
+    set +e
+    GOOS=${GOOS} GOARCH=${GOARCH} go tool compile -V
+    if [[ $? -ne 0 ]]; then
+        echo "compile support for ${GOOS}/${GOARCH} is not provided; skipping"
+        continue
+    fi
+    set -e
     echo "Building  ${GOOS}/${GOARCH}"
     GOOS=${GOOS} GOARCH=${GOARCH} go build  -o /dev/null
  done
